6f79cde904e3ed8627bfa8c690edcf7cb392cfe7
XXXJasmin <113108362+XXXJasmin@users.noreply.github.com>
2023-02-07 12:49:23 +0100

    Add files via upload

--- PRG-kalkulacka/main.py
+++ PRG-kalkulacka/main.py
@@ -51,16 +51,24 @@ class Application(tk.Tk):
         self.listbox.pack(side="left", fill="y")
         self.frame2= tk.Frame(self.frame1)
         self.frame2.pack(side="right", fill="y")
+
         self.button1=tk.Button(self.frame2, text="🡅")
         self.button1.pack(side="top", ipadx=20, ipady=5)
+        self.button1.bind("<ButtonRelease-1>", self.UP)
+
         self.button2=tk.Button(self.frame2, text="🡇")
         self.button2.pack(side="top", ipadx=20, ipady=5)
+        self.button2.bind("<ButtonRelease-1>", self.DOWN)
+
         self.button3=tk.Button(self.frame2, text="delete")
         self.button3.pack(side="bottom", ipadx=20, ipady=5)
+        self.button3.bind("<ButtonRelease-1>", self.DELETE)
+
         self.button4=tk.Button(self.frame2, text="copy")
         self.button4.pack(side="bottom", ipadx=20, ipady=5)
+        self.button4.bind("<ButtonRelease-1>", self.COPY)
 
-        self.label1= tk.Label(self, text=ValueError)
+        self.label1= tk.Label(self, text="")
         self.label1.pack(side="bottom", fill="x",)
 
        
@@ -73,7 +81,27 @@ class Application(tk.Tk):
     def onclick(self, event:tk.Event):
         print(self.listbox.get("anchor"))
         print(self.listbox.get("active"))
-        print(self.listbox.cursorselection())
+        print(self.listbox.curselection())
+
+    def UP(self, event:tk.Event):
+        index = self.listbox.curselection()[0]
+        cislo = self.listbox.get(index)
+        self.listbox.insert(index -1,cislo)
+        self.listbox.delete(index+1)
+
+    def DOWN(self, event:tk.Event):
+        index = self.listbox.curselection()[0]
+        cislo = self.listbox.get(index)
+        self.listbox.insert(index +2,cislo)
+        self.listbox.delete(index)
+
+    def DELETE(self, event:tk.Event):
+        index = self.listbox.curselection()[0]
+        self.listbox.delete(index)
+    def COPY(self, event:tk.Event):
+        index = self.listbox.curselection()[0]
+        cislo = self.listbox.get(index)
+        self.listbox.insert(index +1,cislo)
 
 
     def process(self, e: tk.Event):
@@ -84,23 +112,25 @@ class Application(tk.Tk):
 
     def process_token(self, token:str):
         try:
-            token = token.replace(",", ".")
-            number=float(token)
-            self.listbox.insert(0, number)
-        except ValueError:
-            if token in opr2:
-                a= self.listbox.get(0)
-                self.listbox.delete(0)
-                b= self.listbox.get(0)
-                self.listbox.delete(0)
-                self.listbox.insert(0, opr2[token](a,b))
-            if token in opr1:
-                a= self.listbox.get(0)
-                self.listbox.delete(0)
-                self.listbox.insert(0, opr1[token](a,b))
-            if token in cons:
-                self.listbox.insert(0, cons[token])
-
+            try:
+                token = token.replace(",", ".")
+                number=float(token)
+                self.listbox.insert(0, number)
+            except ValueError:
+                if token in opr2:
+                    a= self.listbox.get(0)
+                    self.listbox.delete(0)
+                    b= self.listbox.get(0)
+                    self.listbox.delete(0)
+                    self.listbox.insert(0, opr2[token](a,b))
+                if token in opr1:
+                    a= self.listbox.get(0)
+                    self.listbox.delete(0)
+                    self.listbox.insert(0, opr1[token](a,b))
+                if token in cons:
+                    self.listbox.insert(0, cons[token])
+        except:
+            self.label1.config(text="chyba")
 
 
         
@@ -111,3 +141,5 @@ class Application(tk.Tk):
 
 app = Application()
 app.mainloop()
+
+
